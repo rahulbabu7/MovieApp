9b6f136302bf71a914e52a21ff6f432a26105053
Harikrishnan-Ashok <harikrishnanashok2001@gmail.com>
2025-07-26 17:28:48 +0530

    Updated App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-const App = ()=>{
-
-    return(
-        <>
-        
-       
-        </>
-    )
+const App = () => {
+	return (
+		<>
+			<h1>2dThis is just test to see the fork changess</h1>
+			<h2>Changed the return in app component thats all</h2>
+			<h2>the return in app component thats all</h2>
+		</>
+	)
 };
 
 export default App;
